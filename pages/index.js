f7d4dfbd31f4ede8aac512c06eb63c858af53819
Morgan Titsworth <Morgan@tobedetermined.dev>
2023-01-06 16:30:51 -0500

    first page drafted structure

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.scss'
+import backround from '../public/backround.png'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,6 +16,26 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
+        <div className={styles.main}>
+          <h1>Morgan Titsworth</h1>
+          <div className={styles.buttonHolder}>
+            <div className={styles.buttonDiv}>
+              <h2>Projects</h2>
+              <svg xmlns="http://www.w3.org/2000/svg" height="50" width="50"><path d="m24 40-2.1-2.15L34.25 25.5H8v-3h26.25L21.9 10.15 24 8l16 16Z"/></svg>
+            </div>            
+            <div className={styles.buttonDiv}>
+              <h2>Contact</h2>
+              <div>
+                <svg xmlns="http://www.w3.org/2000/svg" height="50" width="50"><path d="M24 40 8 24l2.1-2.1 12.4 12.4V8h3v26.3l12.4-12.4L40 24Z"/></svg> 
+              </div>           
+            </div>            
+            <div className={styles.buttonDiv}>
+              <h2>About</h2>
+              <svg xmlns="http://www.w3.org/2000/svg" height="50" width="50"><path d="m24 40-2.1-2.15L34.25 25.5H8v-3h26.25L21.9 10.15 24 8l16 16Z"/></svg>
+            </div>
+          </div>
+        </div>
+        <Image className={styles.backroundImage}src={backround} alt={"A backround image"} fill="true" quality={100}/>
       </main>
     </>
   )
