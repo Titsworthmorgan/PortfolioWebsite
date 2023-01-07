8d0a47ba3d330b5e0b39b99ffe2d78d59fd7c89e
Morgan Titsworth <Morgan@tobedetermined.dev>
2023-01-07 00:34:54 -0500

    Small additions and changes

--- Components/CenterButtons/CenterButton.js
+++ Components/CenterButtons/CenterButton.js
@@ -1,27 +1,35 @@
 import styles from './CenterButton.module.scss'
+import Image from 'next/image'
+import sideways from '../../public/Arrow_usage.png'
+import downwards from '../../public/Arrow_down_usage.png'
+import Link from 'next/link'
 
 export default function CenterButton(){
 
     return(
         <>
-        <div className={styles.buttonDiv}>
-            <h2>Projects</h2>
-            <div>                
-                <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48"><path d="m24 40-2.1-2.15L34.25 25.5H8v-3h26.25L21.9 10.15 24 8l16 16Z"/></svg>
+            <div className={styles.buttonDiv}>
+                <Link href={"/Projects"}>
+                    <h2>Projects</h2>
+                </Link>
+                <div>
+                    <Image src={sideways} alt={"alt Image"} height={48} width={48}/>
+                </div>
+            </div>            
+            <div className={styles.buttonDiv}>
+                <h2>Contact</h2>
+                <div>
+                    <Image src={downwards} alt={"alt Image"} height={48} width={48}/> 
+                </div>           
+            </div>            
+            <div className={styles.buttonDiv}>
+                <Link href={"/About"}>
+                    <h2>About</h2>
+                </Link>
+                <div>
+                    <Image src={sideways} alt={"alt Image"} height={48} width={48}/>
+                </div>
             </div>
-        </div>            
-        <div className={styles.buttonDiv}>
-            <h2>Contact</h2>
-            <div>
-                <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48"><path d="M24 40 8 24l2.1-2.1 12.4 12.4V8h3v26.3l12.4-12.4L40 24Z"/></svg> 
-            </div>           
-        </div>            
-        <div className={styles.buttonDiv}>
-            <h2>About</h2>
-            <div>
-                <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48"><path d="m24 40-2.1-2.15L34.25 25.5H8v-3h26.25L21.9 10.15 24 8l16 16Z"/></svg>
-            </div>
-        </div>
-      </>
+        </>
     )
 }
