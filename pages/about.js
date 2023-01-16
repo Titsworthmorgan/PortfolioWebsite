132411afa3eb18781894e54dfe951c2c9cb6615e
Morgan Titsworth <Morgan@tobedetermined.dev>
2023-01-15 19:34:41 -0500

    asd

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Head from "next/head";
-import Link from "next/link";
 import Image from "next/image";
 import styles from '../styles/pages/about.module.scss'
 import FooterComponent from "../Components/Footer/Footer";
