8cc1770299cedc9e027f574fe6ab84582bcd819d
Morgan Titsworth <Morgan@tobedetermined.dev>
2023-01-15 20:06:12 -0500

    deleted endings

--- pages/about.js
+++ pages/about.js
@@ -3,9 +3,9 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from '../styles/pages/about.module.scss'
 import FooterComponent from "../Components/Footer/Footer";
-import HomePicture from "../public/home-usage-white.png"
-import background from '../public/backround-middle.png'
-import picture from '../public/titsworth-round.png'
+import HomePicture from "../public/home-usage-white"
+import background from '../public/backround-middle'
+import picture from '../public/titsworth-round'
 import { useRouter } from 'next/router'
 
 
--- pages/projects.js
+++ pages/projects.js
@@ -1,8 +1,8 @@
 import Image from "next/image";
 import styles from '../styles/pages/projects.module.scss'
-import background from '../public/backround-sideways.png'
+import background from '../public/backround-sideways'
 import FooterComponent from "../Components/Footer/Footer";
-import HomePicture from "../public/home-usage-white.png"
+import HomePicture from "../public/home-usage-white"
 import Head from "next/head";
 import Link from 'next/link'
 export default function Projects() {
